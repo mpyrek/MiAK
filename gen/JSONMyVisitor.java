04e70213415fecd79e542d5908d2b8b2ff75db7b
mpyrek <monikapyrek782@gmail.com>
2023-01-21 12:07:07 +0100

    Adds all files in one commit :)

--- gen/JSONMyVisitor.java
+++ gen/JSONMyVisitor.java
@@ -1,4 +1,10 @@
 public class JSONMyVisitor extends JSONBaseVisitor<Object>{
+    StringBuffer buffer;
+
+    public JSONMyVisitor() {
+        this.buffer= new StringBuffer();
+    }
+
     /**
      * {@inheritDoc}
      *
@@ -6,8 +12,6 @@ public class JSONMyVisitor extends JSONBaseVisitor<Object>{
      * {@link #visitChildren} on {@code ctx}.</p>
      */
     @Override public Object visitJson(JSONParser.JsonContext ctx) {
-        System.out.println("start");
-
         return visitChildren(ctx);
     }
     /**
@@ -16,7 +20,27 @@ public class JSONMyVisitor extends JSONBaseVisitor<Object>{
      * <p>The default implementation returns the result of calling
      * {@link #visitChildren} on {@code ctx}.</p>
      */
-    @Override public Object visitObj(JSONParser.ObjContext ctx) { return visitChildren(ctx); }
+    @Override public Object visitObj(JSONParser.ObjContext ctx) {
+        for (JSONParser.PairContext pairContext : ctx.pair()) {
+            String temp = "";
+            if (pairContext.value().STRING() != null){
+                temp = "string "+pairContext.STRING().getText().split("\"")[1] +" = "+pairContext.value().STRING() +";\n";
+            } else if (pairContext.value().NUMBER() != null){
+                temp = "int "+pairContext.STRING().getText().split("\"")[1] +" = "+pairContext.value().NUMBER() +";"+"\n";
+            } else if (pairContext.value().arr() != null){
+                temp = "int "+pairContext.STRING().getText().split("\"")[1] + " = [";
+                for (JSONParser.ValueContext valueContext : pairContext.value().arr().value()) {
+                    if (valueContext.NUMBER() != null) temp += valueContext.NUMBER() + ", ";
+                    else if (valueContext.STRING() != null) temp += valueContext.STRING() + ", ";
+                }
+                temp = temp.substring(0,temp.length()-2);
+                temp+="];\n";
+            }
+//            buffer.append("\t");
+            buffer.append(temp);
+        }
+        return visitChildren(ctx);
+    }
     /**
      * {@inheritDoc}
      *
@@ -31,9 +55,6 @@ public class JSONMyVisitor extends JSONBaseVisitor<Object>{
      * {@link #visitChildren} on {@code ctx}.</p>
      */
     @Override public Object visitArr(JSONParser.ArrContext ctx) {
-        for(JSONParser.ValueContext v:ctx.value()){
-            System.out.println(v.STRING() != null ? v.STRING() : v.NUMBER());
-        }
         return visitChildren(ctx);
     }
     /**
--- gen/Launch.java
+++ gen/Launch.java
@@ -4,12 +4,20 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.IOException;
 import static org.antlr.v4.runtime.CharStreams.fromFileName;
 
+import java.io.FileWriter;
+
 public class Launch {
-  
+
+
   public static void main(String[] args){
       CharStream cs = null;
+
+      String filename = "ext_parsed.txt";
+
       try {
-          cs = fromFileName("gen/test.txt");
+          FileWriter fileWriter = new FileWriter(filename);
+
+          cs = fromFileName("gen/ext_test.txt");
           JSONLexer lexer = new JSONLexer(cs);
           CommonTokenStream token = new CommonTokenStream(lexer);
           JSONParser parser = new JSONParser(token);
@@ -18,10 +26,15 @@ public class Launch {
           JSONMyVisitor visitor = new JSONMyVisitor();
 
           visitor.visit(tree);
+
+          fileWriter.append(visitor.buffer);
+
+          fileWriter.close();
+
       } catch (IOException e) {
           throw new RuntimeException(e);
       }
 
   }
-  
 }
+
